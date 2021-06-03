52adcecf699c16bdd722545e10aeba7834c8945c
shinmatsuzaki <smatsuzaki@acm.org>
2021-06-03 22:17:59 +0900

    intial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+#[macro_use]
+extern crate log;
+
+use std::env;
+
+mod tcp_server;
+mod tcp_client;
+mod udp_client;
+mod udp_server;
+
+fn main() {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port]");
+        std::process::exit(1);
+    }
+
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address: &str = &args[3];
+
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                // TCPサーバーの呼び出し
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+            }
+
+            "client" => {
+                // TODO: TCPクライアントの呼び出し
+            }
+
+            _ => {
+              missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                // TODO: UDPサーバーの呼び出し
+            }
+
+            "client" => {
+                // TODO: UDPクライアントの呼び出し
+            }
+
+            _ => {
+                missing_role();
+            }
+        },
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+
+    // 第2引数が不正な時にエラーをを出す関数
+    fn missing_role(){
+        error!("Please specify server or client on the 2nd argument");
+        std::process::exit(1);
+    }
+
+
+
+
+
+
+
+}
---
+++ src/tcp_server.rs
@@ -0,0 +1,28 @@
+use std::{str, thread};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+pub fn serve(address: &str) -> Result<(), failure::Error>{
+    let listner = TcpListener::bind(address)?;
+    loop {
+        let (stream, _) = listner.accept()?;
+        thread::spawn(move || {
+            handler(stream).unwrap_or_else(|error| error!("{:?}", error));
+        });
+    }
+}
+
+fn handler(mut stream: TcpStream) -> Result<(), failure::Error> {
+    debug!("Handling data from {}", stream.peer_addr()?);
+    let mut buffer = [0u8; 1024];
+    loop {
+        let nbytes = stream.read(&mut buffer)?;
+        if nbytes == 0 {
+            debug!("Connection closed");
+            return Ok(());
+        }
+
+        print!("{}", str::from_utf8(&buffer[..nbytes])?);
+        stream.write_all(&buffer[..nbytes])?;
+    }
+}
